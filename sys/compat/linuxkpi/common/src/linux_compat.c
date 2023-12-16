810777bd0869161e58d56c5a0f6876b01ba49b53
Vladimir Kondratyev <wulf@FreeBSD.org>
2023-12-16 18:38:30 +0300

    LinuxKPI: Add devm_device_add_group to linux/device.h
    devm_device_add_group creates a managed attribute group for a device.
    
    Sponsored by:	Serenity Cyber Security, LLC
    MFC after:	1 week

--- sys/compat/linuxkpi/common/src/linux_compat.c
+++ sys/compat/linuxkpi/common/src/linux_compat.c
@@ -2592,6 +2592,36 @@ io_mapping_create_wc(resource_size_t base, unsigned long size)
 	return (io_mapping_init_wc(mapping, base, size));
 }
 
+static void
+devm_device_group_remove(struct device *dev, void *p)
+{
+	const struct attribute_group **dr = p;
+	const struct attribute_group *group = *dr;
+
+	sysfs_remove_group(&dev->kobj, group);
+}
+
+int
+lkpi_devm_device_add_group(struct device *dev,
+    const struct attribute_group *group)
+{
+	const struct attribute_group **dr;
+	int ret;
+
+	dr = devres_alloc(devm_device_group_remove, sizeof(*dr), GFP_KERNEL);
+	if (dr == NULL)
+		return (-ENOMEM);
+
+	ret = sysfs_create_group(&dev->kobj, group);
+	if (ret == 0) {
+		*dr = group;
+		devres_add(dev, dr);
+	} else
+		devres_free(dr);
+
+	return (ret);
+}
+
 #if defined(__i386__) || defined(__amd64__)
 bool linux_cpu_has_clflush;
 struct cpuinfo_x86 boot_cpu_data;
