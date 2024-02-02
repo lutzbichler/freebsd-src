7add27102fba26e55099660a01d7a742cfd14b6c
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-02-02 19:50:52 +0100

    LinuxKPI: Support get_user_pages signature change for Linux 6.5

--- sys/compat/linuxkpi/common/src/linux_page.c
+++ sys/compat/linuxkpi/common/src/linux_page.c
@@ -277,7 +277,7 @@ get_user_pages_remote(struct task_struct *task, struct mm_struct *mm,
 }
 
 long
-get_user_pages(unsigned long start, unsigned long nr_pages,
+linux_get_user_pages(unsigned long start, unsigned long nr_pages,
     unsigned int gup_flags, struct page **pages, struct vm_area_struct **vmas)
 {
 	vm_map_t map;
