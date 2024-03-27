f3d6684a155ff10c3deb2abfb03b8b098af255f5
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-03-27 19:27:42 +0100

    LinuxKPI: Added <linux/pagewalk.h>

---
+++ sys/compat/linuxkpi/common/include/linux/pagewalk.h
@@ -0,0 +1,13 @@
+/* Public domain */
+
+#ifndef LINUXKPI_PAGE_WALK_H_
+#define LINUXKPI_PAGE_WALK_H_
+
+#include <linux/mm.h>
+
+enum page_walk_action {
+	dummy
+};
+
+#endif /* LINUXKPI_PAGE_WALK_H_ */
+
