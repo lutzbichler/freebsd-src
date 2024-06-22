9a287594c668d416c5d882154884d6aa73e963c2
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-06-22 11:40:32 +0200

    LinuxKPI: Add for_each_[clear|set]_bitrange functions
    Add <linux/find.h> and include it from <linux/bitmap.h>

---
+++ sys/compat/linuxkpi/common/include/linux/find.h
@@ -0,0 +1,24 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LINUXKPI_FIND_H
+#define _LINUXKPI_FIND_H
+
+#include <linux/bitops.h>
+
+#define for_each_clear_bitrange(start, end, addr, size) \
+        for (start = 0; \
+             start = find_next_zero_bit(addr, size, start), \
+             end = find_next_bit(addr, size, start + 1), \
+             start < size; \
+             start = end + 1)
+
+#define for_each_set_bitrange(start, end, addr, size) \
+        for (start = 0; \
+             start = find_next_bit(addr, size, start), \
+             end = find_next_bit(addr, size, start + 1), \
+             start < size; \
+             start = end + 1)
+
+#endif /* _LINUXKPI_FIND_H */
