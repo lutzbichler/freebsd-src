2d000d716e02fd05489e5b1a4d95bb9fa7ef390e
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-06-22 11:52:01 +0200

    LinuxKPI: Add pci_sriov_get_totalvfs (dummy)

--- sys/compat/linuxkpi/common/include/linux/pci.h
+++ sys/compat/linuxkpi/common/include/linux/pci.h
@@ -1541,4 +1541,11 @@ pci_pr3_present(struct pci_dev *pdev)
 	return (0);
 }
 
+static inline int
+pci_sriov_get_totalvfs(struct pci_dev *pdev)
+{
+	
+	return (0);
+}
+
 #endif	/* _LINUXKPI_LINUX_PCI_H_ */
