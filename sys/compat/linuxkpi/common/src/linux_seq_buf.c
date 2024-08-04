46704c7dc2ce32934d93622a7bb01aac24d0c827
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-08-04 20:37:00 +0200

    LinuxKPI: Add seq_buf

---
+++ sys/compat/linuxkpi/common/src/linux_seq_buf.c
@@ -0,0 +1,55 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024, Lutz Bichler <lutz.bichler@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/param.h>
+
+#include <linux/seq_buf.h>
+
+void
+lkpi_seq_buf_vprintf(struct seq_buf *s, const char *fmt, va_list args)
+{
+	int ret;
+
+	ret = snprintf(s->buf, s->len, fmt, args);
+	if (ret == 0)
+		s->cur = strlen(s->buf);
+	else
+		s->cur = s->len + 1; // overflow
+}
+
+void
+lkpi_seq_buf_printf(struct seq_buf *s, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	lkpi_seq_buf_vprintf(s, fmt, args);
+	va_end(args);
+}
