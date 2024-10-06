40cbffdec1b949867fe601676c928b7621bc0f75
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-10-06 16:28:53 +0200

    LinuxKPI: Add mem_is_zero, strtomem_pad and strim functions

--- sys/compat/linuxkpi/common/include/linux/string.h
+++ sys/compat/linuxkpi/common/include/linux/string.h
@@ -308,4 +308,39 @@ memzero_explicit(void *p, size_t s)
 	__asm__ __volatile__("": :"r"(p) :"memory");
 }
 
+static inline bool
+mem_is_zero(void *p, size_t s)
+{
+	return (NULL == memchr_inv(p, 0, s));
+}
+
+static inline void
+strtomem_pad(void *dst, const void *src, int ch)
+{
+	size_t dstlen = __builtin_object_size(dst, 1);
+	size_t srclen = __builtin_object_size(src, 1);
+
+	memcpy_and_pad(dst, dstlen, src, srclen, ch);
+}
+
+static inline char*
+strim(char *src)
+{
+	char *s;
+	size_t size;
+
+	size = strlen(src);
+	if (0 == size)
+		return (src);
+
+	s = src + size - 1;
+	while (s >= src && isspace(*s)) s--;
+	*(s + 1) = '\0';
+
+	s = src;
+	while (isspace(*s)) s++;
+
+	return (s);
+}
+
 #endif	/* _LINUXKPI_LINUX_STRING_H_ */
