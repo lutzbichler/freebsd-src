bcfcb138f602a8f5fa85794974d3d2d40f7f39f7
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-10-27 12:18:10 +0100

    LinuxKPI: Add irq.h

---
+++ sys/compat/linuxkpi/common/include/linux/irq.h
@@ -0,0 +1,14 @@
+/* Public domain */
+
+#ifndef LINUXKPI_IRQ_H_
+#define LINUXKPI_IRQ_H_
+
+static inline int
+generic_handle_irq(unsigned int irq)
+{
+
+	return (0);
+}
+
+#endif /* LINUXKPI_IRQ_H_ */
+
