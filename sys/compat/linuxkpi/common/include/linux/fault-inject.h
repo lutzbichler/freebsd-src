0ebd491751bad2b82f37e1f78e419c2b0619b5b1
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-10-27 12:17:25 +0100

    LinuxKPI: Add fault-inject.h

---
+++ sys/compat/linuxkpi/common/include/linux/fault-inject.h
@@ -0,0 +1,13 @@
+/* Public domain */
+
+#ifndef LINUXKPI_FAULT_INJECT_H_
+#define LINUXKPI_FAULT_INJECT_H_
+
+#define	DECLARE_FAULT_ATTR(attr)
+
+#define	should_fail(attr, size)	false
+
+#define	fault_create_debugfs_attr(name, parent, attr)
+
+#endif /* LINUXKPI_FAULT_INJECT_H_ */
+
