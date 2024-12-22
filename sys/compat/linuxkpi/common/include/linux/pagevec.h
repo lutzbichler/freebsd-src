fcc6748f3e962c5031724b62498a5adc70a96052
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2024-12-22 15:46:28 +0100

    linuxkpi: Add `folio` and `folio_batch` APIs
    [Why]
    They are used by the i915 DRM driver in Linux 6.6 (although this changes
    was only backported with Linux 6.7 DRM drivers...).
    
    [How]
    `struct folio` simply wraps `struct page` for now.
    
    `struct folio_batch` is the same as `struct pagevec` but it works with
    `struct folio` instead of `struct page` directly.

--- sys/compat/linuxkpi/common/include/linux/pagevec.h
+++ sys/compat/linuxkpi/common/include/linux/pagevec.h
@@ -66,4 +66,60 @@ check_move_unevictable_pages(struct pagevec *pvec)
 {
 }
 
+/* ------------------------------------------------------------------- */
+
+struct folio;
+
+struct folio_batch {
+	uint8_t	nr;
+	struct folio *folios[PAGEVEC_SIZE];
+};
+
+static inline void
+folio_batch_init(struct folio_batch *fbatch)
+{
+	fbatch->nr = 0;
+}
+
+static inline void
+folio_batch_reinit(struct folio_batch *fbatch)
+{
+	fbatch->nr = 0;
+}
+
+static inline unsigned int
+folio_batch_count(struct folio_batch *fbatch)
+{
+	return fbatch->nr;
+}
+
+static inline unsigned int
+folio_batch_space(struct folio_batch *fbatch)
+{
+	return PAGEVEC_SIZE - fbatch->nr;
+}
+
+static inline unsigned int
+folio_batch_add(struct folio_batch *fbatch, struct folio *folio)
+{
+	fbatch->folios[fbatch->nr++] = folio;
+	return PAGEVEC_SIZE - fbatch->nr;
+}
+
+static inline void
+__folio_batch_release(struct folio_batch *fbatch)
+{
+	release_pages(
+	    (struct page **)fbatch->folios,
+	    folio_batch_count(fbatch));
+	folio_batch_reinit(fbatch);
+}
+
+static inline void
+folio_batch_release(struct folio_batch *fbatch)
+{
+	if (folio_batch_count(fbatch))
+		__folio_batch_release(fbatch);
+}
+
 #endif	/* _LINUXKPI_LINUX_PAGEVEC_H_ */
