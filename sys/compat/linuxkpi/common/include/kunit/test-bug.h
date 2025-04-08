05663941a5952c8971236b075fa4ad62c2fba837
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-04-08 22:08:25 +0200

    LinuxKPI: Add <kunit/test-bug.h>

---
+++ sys/compat/linuxkpi/common/include/kunit/test-bug.h
@@ -0,0 +1,11 @@
+/* Public domain */
+
+#ifndef LINUXKPI_KUNIT_TEST_BUG_H_
+#define LINUXKPI_KUNIT_TEST_BUG_H_
+
+static inline void
+kunit_fail_current_test(const char *name)
+{
+}
+
+#endif /* LINUXKPI_KUNIT_TEST_BUG_H_ */
