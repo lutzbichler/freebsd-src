0e04546538f3adda307d5c7792f13e1f290728e2
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-04-13 10:08:37 +0200

    LinuxKPI: Add LKPI_DEFINE_CLASS, LKPI_CLASS

--- sys/compat/linuxkpi/common/include/linux/cleanup.h
+++ sys/compat/linuxkpi/common/include/linux/cleanup.h
@@ -43,4 +43,20 @@
     guard_ ## _n ## _t guard_ ## _n ## _ ## __COUNTER__			\
 	__cleanup(guard_ ## _n ## _destroy) = guard_ ## _n ## _create
 
+#define LKPI_DEFINE_CLASS(_n, _t, _exit, _init, _args...)		\
+typedef _t lkpi_##_n;							\
+static inline void lkpi_##_n##_dtor(_t *obj)				\
+{									\
+    _t _T = *obj; _exit;						\
+}									\
+static inline _t lkpi_##_n##_ctor(_args)				\
+{									\
+    _t obj = _init;							\
+    return obj;								\
+}
+
+#define LKPI_CLASS(_n, _v)						\
+        lkpi_##_n _v __cleanup(lkpi_##_n##_dtor) = lkpi_##_n##_ctor
+
+
 #endif	/* _LINUXKPI_LINUX_CLEANUP_H */
