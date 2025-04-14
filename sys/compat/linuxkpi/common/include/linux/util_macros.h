596edfe82eaf1bb3aca2f61e952f8830590771c0
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-04-14 19:47:42 +0200

    LinuxKPI: Add for_each_if to <linux/util_macros.h>

---
+++ sys/compat/linuxkpi/common/include/linux/util_macros.h
@@ -0,0 +1,9 @@
+/* Public domain */
+
+#ifndef _LINUXKPI_LINUX_UTIL_MACROS_H_
+#define _LINUXKPI_LINUX_UTIL_MACROS_H_
+
+#define for_each_if(cond) if (!(cond)) {} else
+
+#endif /* _LINUXKPI_LINUX_UTIL_MACROS_H_ */
+
