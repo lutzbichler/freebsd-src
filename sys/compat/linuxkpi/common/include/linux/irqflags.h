91bd866a7c83eeb512376a7b343517594b7271c2
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-04-18 08:41:23 +0200

    LinuxKPI: Move local_irq_(save|restore) to <linux/irqflags.h>

---
+++ sys/compat/linuxkpi/common/include/linux/irqflags.h
@@ -0,0 +1,14 @@
+/* Public domain */
+
+#ifndef LINUXKPI_IRQFLAGS_H_
+#define LINUXKPI_IRQFLAGS_H_
+
+#define	local_irq_save(flags) do {		\
+	(flags) = 0;				\
+} while (0)
+
+#define	local_irq_restore(flags) do {		\
+	(void)(flags);				\
+} while (0)
+
+#endif /* LINUXKPI_IRQFLAGS_H_ */
