4aad888027a1ada996b0577aedfa65c5f71199a7
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-07-21 19:11:08 +0200

    LinuxKPI: Add ratelimit_state_reset_interval

--- sys/compat/linuxkpi/common/include/linux/ratelimit.h
+++ sys/compat/linuxkpi/common/include/linux/ratelimit.h
@@ -13,6 +13,7 @@ struct ratelimit_state {
 
 #define ratelimit_state_init(x, y, z)
 #define ratelimit_set_flags(x, y)
+#define ratelimit_state_reset_interval(x, y)
 
 #define WARN_RATELIMIT(condition, format, ...) \
 	WARN(condition, format, ##__VA_ARGS__)
