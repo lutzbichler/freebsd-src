0c7a443585230e50f86539e9405e4710b09d4396
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-07-22 22:00:21 +0200

    LinuxKPI: Rename from_timer to timer_container for LINUXKPI_VERSION>=61600

--- sys/compat/linuxkpi/common/include/linux/timer.h
+++ sys/compat/linuxkpi/common/include/linux/timer.h
@@ -49,8 +49,13 @@ extern unsigned long linux_timer_hz_mask;
 
 #define	TIMER_IRQSAFE	0x0001
 
-#define	from_timer(var, arg, field)					\
+#if defined(LINUXKPI_VERSION) && LINUXKPI_VERSION >= 61600
+#define timer_container_of(var, arg, field)				\
         container_of(arg, typeof(*(var)), field)
+#else
+#define from_timer(var, arg, field)					\
+	container_of(arg, typeof(*(var)), field)
+#endif
 
 #define	timer_setup(timer, func, flags) do {				\
 	CTASSERT(((flags) & ~TIMER_IRQSAFE) == 0);			\
