803a575fe972c0d333a180d3c40b4cabf085fcc0
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-08-10 12:03:09 +0200

    LinuxKPI: Add string_get_size
    Used by drm/xe driver. Not implemented yet.

--- sys/compat/linuxkpi/common/include/linux/string_helpers.h
+++ sys/compat/linuxkpi/common/include/linux/string_helpers.h
@@ -9,4 +9,17 @@
 
 #include <linux/string_choices.h>
 
+enum string_size_units {
+        STRING_UNITS_10,
+        STRING_UNITS_2,
+};
+
+static inline int
+string_get_size(uint64_t size, uint64_t blk_size, enum string_size_units units,
+		char* buf, int len)
+{
+	buf[0] = '\0';
+	return (-1);
+}
+
 #endif
