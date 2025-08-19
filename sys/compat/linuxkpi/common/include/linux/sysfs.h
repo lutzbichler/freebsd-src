ab68b4a47469c1e955042421d67d9768b79f28d3
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-08-19 22:46:52 +0200

    LinuxKPI: Change signature for bin_attribute::read()/write()
    It is changed for LINUXKPI_VERSION > 61600. Linux has done this
    change between v6.15 and v6.16, but the drm driver were still
    using bin_attribute::read_new()/write_new()

--- sys/compat/linuxkpi/common/include/linux/sysfs.h
+++ sys/compat/linuxkpi/common/include/linux/sysfs.h
@@ -53,10 +53,17 @@ struct attribute_group {
 struct bin_attribute {
 	struct attribute	attr;
 	size_t			size;
+#if defined(LINUXKPI_VERSION) && LINUXKPI_VERSION < 61700
 	ssize_t (*read)(struct linux_file *, struct kobject *,
 			struct bin_attribute *, char *, loff_t, size_t);
 	ssize_t (*write)(struct linux_file *, struct kobject *,
 			 struct bin_attribute *, char *, loff_t, size_t);
+#else
+        ssize_t (*read)(struct linux_file *, struct kobject *,
+                        const struct bin_attribute *, char *, loff_t, size_t);
+        ssize_t (*write)(struct linux_file *, struct kobject *,
+                         const struct bin_attribute *, char *, loff_t, size_t);
+#endif
 	ssize_t (*read_new)(struct linux_file *, struct kobject *,
 			const struct bin_attribute *, char *, loff_t, size_t);
 	ssize_t (*write_new)(struct linux_file *, struct kobject *,
