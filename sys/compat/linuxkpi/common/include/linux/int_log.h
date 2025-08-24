0a32a4af4769a6d85e21461518cd1175da743c0d
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-08-24 19:20:16 +0200

    LinuxKPI: Add intlog2, intlog10

---
+++ sys/compat/linuxkpi/common/include/linux/int_log.h
@@ -0,0 +1,9 @@
+/* Public domain */
+
+#ifndef LINUXKPI_INT_LOG_H
+#define LINUXKPI_INT_LOG_H
+
+unsigned int intlog2(uint32_t v);
+unsigned int intlog10(uint32_t v);
+
+#endif /* LINUKPI_INT_LOG_H */
