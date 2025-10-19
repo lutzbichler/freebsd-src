da84df99cc3f9479058c9f546bb08a0082b755d7
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-10-19 13:35:06 +0200

    LinuxKPI: Isolate enum dma_data_direction into dma-direction.h

--- sys/compat/linuxkpi/common/include/linux/dma-mapping.h
+++ sys/compat/linuxkpi/common/include/linux/dma-mapping.h
@@ -33,6 +33,7 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/dma-attrs.h>
+#include <linux/dma-direction.h>
 #include <linux/scatterlist.h>
 #include <linux/mm.h>
 #include <linux/page.h>
@@ -47,13 +48,6 @@
 #include <vm/pmap.h>
 
 #include <machine/bus.h>
-
-enum dma_data_direction {
-	DMA_BIDIRECTIONAL = 0,
-	DMA_TO_DEVICE = 1,
-	DMA_FROM_DEVICE = 2,
-	DMA_NONE = 3,
-};
 
 struct dma_map_ops {
 	void* (*alloc_coherent)(struct device *dev, size_t size,
