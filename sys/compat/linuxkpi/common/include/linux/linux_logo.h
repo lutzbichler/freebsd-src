31dd130b50e71b37bce6562f07fa72324988f890
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-01-31 12:53:34 +0100

    LinuxKPI: Adding <linux/linux_logo.h>
    Adding struct linux_logo. This is used by the DRM panic code in
    Linux v6.11.

---
+++ sys/compat/linuxkpi/common/include/linux/linux_logo.h
@@ -0,0 +1,13 @@
+/* Public domain */
+
+#ifndef LINUXKPI_LINUX_LINUX_LOGO_H_
+#define LINUXKPI_LINUX_LINUX_LOGO_H_
+
+struct linux_logo {
+	int width;
+	int height;
+	void *data;
+};
+
+#endif /* LINUXKPI_LINUX_LINUX_LOGO_H_ */
+
