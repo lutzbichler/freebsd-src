d3d88802bdae6aa231202739b4085f5a141ca1d2
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-01-31 22:42:10 +0100

    LinuxKPI: Added kmap_local_page_try_from_panic

--- sys/compat/linuxkpi/common/include/linux/highmem.h
+++ sys/compat/linuxkpi/common/include/linux/highmem.h
@@ -105,6 +105,12 @@ kmap_local_page_prot(struct page *page, pgprot_t prot)
 	return (kmap_atomic_prot(page, prot));
 }
 
+static inline void *
+kmap_local_page_try_from_panic(struct page *page)
+{
+	return (kmap(page));
+}
+
 static inline void
 kunmap(struct page *page)
 {
