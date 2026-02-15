97642b0e2c59d1c0de9939bbbbadb51f4d7ba508
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-02-15 18:30:55 +0100

    LinuxKPI: Fix usage of __COUNTER__
    Replace direct concatenations by calls to __CONCAT

--- sys/compat/linuxkpi/common/include/linux/cleanup.h
+++ sys/compat/linuxkpi/common/include/linux/cleanup.h
@@ -76,7 +76,7 @@
 #define	_guard(_n, _x)							\
     DECLARE(_n, _x)
 #define	guard(_n)							\
-    _guard(_n, guard_ ## _n ## _ ## __COUNTER__)
+    _guard(_n, __CONCAT(guard_, __CONCAT(_n, __CONCAT(_, __COUNTER__))))
 
 #define	DEFINE_FREE(_n, _t, _f)						\
     static inline void							\
@@ -185,6 +185,6 @@ _l:									\
 		} else
 
 #define	scoped_guard(_n, ...)						\
-    _scoped_guard(_n, ___label_ ## __COUNTER__, ##__VA_ARGS__)
+    _scoped_guard(_n, __CONCAT(___label_, __COUNTER__), ##__VA_ARGS__)
 
 #endif	/* _LINUXKPI_LINUX_CLEANUP_H */
