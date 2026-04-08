b29e7ef41bf456d46c9e0ce318c0410daa0c7e1f
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-04-08 10:41:41 +0200

    LinuxKPI: Add <linux/irqflags.h>
    - Move local_irq_(save|restore) from <linux/spinlock.h>
    - Add (empty) definitions of local_irq_(enable|disable)
    - Include <linux/irqflags> from <linux/spinlock.h>

--- sys/compat/linuxkpi/common/include/linux/spinlock.h
+++ sys/compat/linuxkpi/common/include/linux/spinlock.h
@@ -38,6 +38,7 @@
 
 #include <linux/cleanup.h>
 #include <linux/compiler.h>
+#include <linux/irqflags.h>
 #include <linux/rwlock.h>
 #include <linux/bottom_half.h>
 #include <linux/lockdep.h>
@@ -154,14 +155,6 @@ typedef struct mtx spinlock_t;
 	if (SPIN_SKIP())			\
 		break;				\
 	mtx_assert(_l, MA_OWNED);		\
-} while (0)
-
-#define	local_irq_save(flags) do {		\
-	(flags) = 0;				\
-} while (0)
-
-#define	local_irq_restore(flags) do {		\
-	(void)(flags);				\
 } while (0)
 
 #define	atomic_dec_and_lock_irqsave(cnt, lock, flags) \
