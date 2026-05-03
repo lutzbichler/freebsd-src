2f70ff91672b1fac3c409db153709136b1e0b766
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-05-03 11:44:20 +0200

    LinuxKPI: Added generic_handle_irq to <linux/irq.h>

---
+++ sys/compat/linuxkpi/common/include/linux/irq.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 Lutz Bichler
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LINUXKPI_IRQ_H_
+#define _LINUXKPI_IRQ_H_
+
+static inline int
+generic_handle_irq(unsigned int irq)
+{
+
+	return (0);
+}
+
+#endif /* _LINUXKPI_IRQ_H_ */
