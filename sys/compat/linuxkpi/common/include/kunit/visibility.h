2f21153a8ee0501bb7553586de7f8c975032790f
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-05-03 15:19:41 +0200

    LinuxKPI: Added <kunit/visibility.h>
    - defines (empty) EXPORT_SYMBOL_IF_KUNIT
    
    Used by drm/xe driver.

---
+++ sys/compat/linuxkpi/common/include/kunit/visibility.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Lutz Bichler
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LINUXKPI_KUNIT_VISIBILITY_H_
+#define _LINUXKPI_KUNIT_VISIBILITY_H_
+
+#define	EXPORT_SYMBOL_IF_KUNIT(x)
+
+#endif /* _LINUXKPI_KUNIT_VISBILITY_H_ */
