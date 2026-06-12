8981ac2a5e2fe7b0d23aa7fb3a3d27a7b9e20af5
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-06-12 22:21:12 +0200

    linuxkpi: Define `COUNT_ARGS()` and `CONCATENATE()`
    `COUNT_ARGS()` counts the number of arguments it is passed. The
    implementation is heavily inspired from the one of `CTR()`.
    
    `CONCATENATE()` is an alias for `__CONCAT()`.
    
    Reviewed by:
    Sponsored by:   The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D57584

--- sys/compat/linuxkpi/common/include/linux/args.h
+++ sys/compat/linuxkpi/common/include/linux/args.h
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2026 Lutz Bichler
- *
  * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 The FreeBSD Foundation
  */
 
-#ifndef LINUXKPI_ARGS_H_
-#define LINUXKPI_ARGS_H_
+#ifndef	_LINUXKPI_LINUX_ARGS_H_
+#define	_LINUXKPI_LINUX_ARGS_H_
 
-#include <sys/cdefs.h>
+#define	__COUNT_ARGS(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, __count, ...)	\
+    __count
+#define	COUNT_ARGS(X...) \
+    __COUNT_ARGS(, ##X, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
-#define LKPI_COUNT_ARGS(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _n, X...) _n
-#define COUNT_ARGS(X...) LKPI_COUNT_ARGS(, ##X, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define	CONCATENATE(a, b)	__CONCAT(a, b)
 
-#define CONCATENATE(x, y) __CONCAT(x, y)
-
-#endif /* LINUXKPI_ARGS_H_ */
+#endif /* _LINUXKPI_LINUX_ARGS_H_ */
