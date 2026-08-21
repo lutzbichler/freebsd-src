db11b6f7ae67353bf6d548dea6877f6a0838afcd
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-08-21 21:31:22 +0200

    LinuxKPI: Add <linux/ratelimit_types.h>
    Defines __ratelimit and ___ratelimit.
    
    Used by drm/xe driver in Linux v6.19

---
+++ sys/compat/linuxkpi/common/include/linux/ratelimit_types.h
@@ -0,0 +1,10 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ */
+#ifndef _LINUXKPI_LINUX_RATELIMIT_TYPES_H
+#define _LINUXKPI_LINUX_RATELIMIT_TYPES_H
+
+#define	___ratelimit(x, y)	(false)
+
+#endif /* _LINUXKPI_LINUX_RATELIMIT_TYPES_H */
