b85fca4dd5a274fbff970abb2b787ae8e6abe897
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-08-22 15:07:07 +0200

    LinuxKPI: Add error codes EKEYREJECTED, EUCLEAN and ESTRPIPE

--- sys/compat/linuxkpi/common/include/linux/errno.h
+++ sys/compat/linuxkpi/common/include/linux/errno.h
@@ -71,5 +71,8 @@
 #define	ENAVAIL		535
 #define	ENOPKG		536
 #define	EREMCHG		537
+#define	EKEYREJECTED    538
+#define	EUCLEAN		539
+#define	ESTRPIPE	540
 
 #endif					/* _LINUXKPI_LINUX_ERRNO_H_ */
