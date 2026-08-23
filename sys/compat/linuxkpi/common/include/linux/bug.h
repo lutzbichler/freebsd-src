1cd66f17724df6b0693ca5b05808524e1f6fb9c9
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-08-23 09:37:10 +0200

    LinuxKPI: Move <linux/bug.h> to common and include <linux/build_bug.h>
    Needed by drm_mm in Linux v6.19

---
+++ sys/compat/linuxkpi/common/include/linux/bug.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 Lutz Bichler
+ */
+#ifndef _LINUXKPI_LINUX_BUG_H
+#define _LINUXKPI_LINUX_BUG_H
+
+#include <linux/build_bug.h>
+
+#endif /* _LINUXKPI_LINUX_BUG_H */
