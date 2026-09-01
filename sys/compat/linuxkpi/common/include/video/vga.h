7b98abe42963e93fb80045a5f557531950660b5d
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-09-01 22:28:25 +0200

    LinuxKPI: Add VGA definitions
    - VGA_IS0_R
    - VGA_MIS_COLOR
    - VGA_MIS_ENB_MEM_ACCESS
    
    Used by drm/i915 and drm/xe drivers in Linux v7.1

--- sys/compat/linuxkpi/common/include/video/vga.h
+++ sys/compat/linuxkpi/common/include/video/vga.h
@@ -11,9 +11,14 @@
 #define VGA_SEQ_D	0x3c5
 #define VGA_MIS_R	0x3cc
 
+#define VGA_IS0_R	0x3c2 
+
 #define VGA_SR01_SCREEN_OFF	(1 << 5)
 
 #define VGA_FB_PHYS_BASE	0xA0000
 #define VGA_FB_PHYS_SIZE	65536
+
+#define VGA_MIS_COLOR           0x1
+#define VGA_MIS_ENB_MEM_ACCESS  0x2
 
 #endif
